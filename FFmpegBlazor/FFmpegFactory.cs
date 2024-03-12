e9517a1eb352a44207b6e9b69ebfab5be6321285
Shivendra  Singh <45932883+sps014@users.noreply.github.com>
2024-03-12 12:51:17 +0530

    init

---
+++ FFmpegBlazor/FFmpegFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpegBlazor
+{
+    public static class FFmpegFactory
+    {
+        public static async void Init(IJSRuntime Runtime)
+        {
+            var obj = await Runtime.InvokeAsync<IJSObjectReference>("import", "./_content/FFmpegBlazor/js/index.js");
+        }
+    }
+}
--- FFmpegBlazor/JS/src/index.js
+++ FFmpegBlazor/JS/src/index.js
@@ -1,3 +1,3 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 
-console.log(FFmpeg);
+l
